6028a8db25305f7bb3e33e92e7038ff395647230
Daniel Silva <daniel.raizon@gmail.com>
2022-08-30 08:09:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div class="bg-gray-400">
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + TailwindCSS" />
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  defineProps<{ msg: string }>();
+
+  const count = ref(0);
+</script>
+
+<template>
+  <div class="bg-neutral-50 my-10 p-10 flex flex-col items-center">
+    <h1 class="text-3xl">{{ msg }}</h1>
+
+    <button class="bg-indigo-500" type="button" @click="count++">
+      count is: {{ count }}
+    </button>
+  </div>
+</template>
