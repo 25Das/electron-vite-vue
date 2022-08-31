c30e6f90a63ac30e8d946470ed14d928b229049f
Daniel Silva <daniel.raizon@gmail.com>
2022-08-31 08:55:07 -0300

    Add vuex store

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 import router from './router';
+import store from './store';
 // import './samples/node-api'
 
 createApp(App)
-.use(router)
+  .use(router)
+  .use(store)
   .mount('#app')
   .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+    postMessage({ payload: 'removeLoading' }, '*');
+  });
